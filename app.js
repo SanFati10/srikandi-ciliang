9935a2aaa0907b8ca519057ae665210b87648da3
SanFati10 <ihsanulfatih@gmail.com>
2026-08-19 19:53:14 +0700

    Update data UMKM Desa Ciliang

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Koneksi Database Supabase (PostgreSQL)
 const pool = new Pool({
